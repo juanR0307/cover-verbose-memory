c5116cb5a682af82ce695abc222d430aeca975a6
juanR0307 <201980031@psu.palawan.edu>
2022-02-24 19:02:13 +0800

    inc directory done

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php include 'inc/header.php'; ?>
+<header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link " href="index.php">Home</a>
+            <a class="nav-link" href="features.php">Features</a>
+            <a class="nav-link active" href="contact.php">Contact</a>
+
+          </nav>
+        </div>
+  </header>
+<p>Successfully navigated to Contacts page</p>
+<?php include 'inc/footer.php'; ?>
---
+++ features.php
@@ -0,0 +1,14 @@
+<?php include 'inc/header.php'; ?>
+<header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link " href="index.php">Home</a>
+            <a class="nav-link active" href="features.php">Features</a>
+            <a class="nav-link" href="contact.php">Contact</a>
+
+          </nav>
+        </div>
+  </header>
+  <p>Successfully navigated to Feature page</p>
+<?php include 'inc/footer.php'; ?>
